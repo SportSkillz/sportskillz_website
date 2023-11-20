1177b181b4a8a78dfeba643c541f505d03fe454f
Erick Gomes <110913912+ErickGms01@users.noreply.github.com>
2023-11-20 03:36:15 -0300

    modifing signIn&signUp

---
+++ public/script/login/signup.js
@@ -0,0 +1,12 @@
+function togglePassword() {
+    var passwordInput = document.getElementById("password");
+    var toggleIcon = document.querySelector(".toggle-password");
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        toggleIcon.textContent = "Hide";
+    } else {
+        passwordInput.type = "password";
+        toggleIcon.textContent = "Show";
+    }
+}
