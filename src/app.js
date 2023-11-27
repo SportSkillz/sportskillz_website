9d031b08e550e5e1ab9708b433d322a3cd25349d
Paulo Roberto <paulo.rcarlos23@gmail.com>
2023-11-27 09:50:36 -0300

    resolvido erro: 'ReferenceError: __dirname is not defined in ES module scope'

--- src/app.js
+++ src/app.js
@@ -3,6 +3,11 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
