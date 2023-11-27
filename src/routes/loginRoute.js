42647cffe878056b7638d2454cf598ac6391460a
Paulo Roberto <paulo.rcarlos23@gmail.com>
2023-11-27 09:37:27 -0300

    Função createUser adicionada na rota 'singUpRoute'

--- src/routes/loginRoute.js
+++ src/routes/loginRoute.js
@@ -1,13 +1,9 @@
 import express from 'express';
-import {createUser} from '../controllers/userController'
 var router = express.Router();
 
 router.get('/', (req, res) => {
     res.render('login/login.ejs')
-});
+})
 
-router.post('/', (req, res) => {
-
-});
 
 export default router; 
--- src/routes/signUpRoute.js
+++ src/routes/signUpRoute.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import { createUser } from '../controllers/userController';
 var router = express.Router();
 
 router.get('/', (req, res) => {
     res.render('login/signup.ejs')
 })
+
+router.post('/', createUser);
 
 export default router; 
