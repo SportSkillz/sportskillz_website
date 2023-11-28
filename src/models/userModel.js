d476b02dbb0562074ae7faca1936f98ea1a49794
Paulo Roberto <paulo.rcarlos23@gmail.com>
2023-11-28 10:23:15 -0300

    Criada função que cryptografa a senha antes de armazená-la no banco de dados

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import '../app.js';
+import bcrypt from 'bcrypt';
 
 const {Schema} = mongoose;
 
@@ -7,8 +8,18 @@ const userSchema = new Schema({
     name: {type: String, require: true},
     email: {type: String, require: true},
     password: {type: String, require: true}
-
 });
+
+//Realiza a cryptografia da senha, para proteção do uuário
+userSchema.pre('save', async function(next){
+    try {
+        const salt = await bcrypt.genSalt(14);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+})
 
 const UserModel = mongoose.model('User', userSchema);
 
