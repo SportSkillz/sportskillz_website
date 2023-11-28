6898c2b94811785ebd9d2c46aa6ecab2fb0f1cd9
Paulo Roberto <paulo.rcarlos23@gmail.com>
2023-11-28 19:20:20 -0300

    Criando arquivo de configuração do passport

---
+++ src/controllers/passportLocal.js
@@ -0,0 +1,31 @@
+import passport from "passport";
+import { Strategy as LocalStrategy} from "passport-local";
+import user from "../models/userModel.js"
+
+passport.use(new LocalStrategy( { usernameField: 'email', session: false},
+    async (email, password, done) => {
+        try{
+            const foundUser = await user.findOne({ email: email});
+            if(!foundUser){ return done(null, false)};
+            if(!foundUser.isValidPassword(password)) { return done(null, false)};
+            return done(null, foundUser);
+        } catch(err){
+            return done(err);
+        }
+    }
+    
+));
+
+passport.serializeUser(function(user, cb) {
+    process.nextTick(function() {
+      cb(null, { id: user.id, username: user.username });
+    });
+  });
+  
+  passport.deserializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, user);
+    });
+  });
+
+export default passport;
