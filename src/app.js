ab02e69c71050419f9d03257ebb6d55da2888141
Erick Gomes <110913912+erickgms01@users.noreply.github.com>
2023-11-29 07:54:08 -0300

    atualizando caminho do public

--- src/app.js
+++ src/app.js
@@ -1,44 +1,47 @@
-//Importando módulo dotenv
-import  dotenv from "dotenv";
+// Importando módulo dotenv
+import dotenv from 'dotenv';
 dotenv.config();
 
-//Definindo constantes
+// Importando módulos
 import express from 'express';
+import path from 'path';
+import mongoose from 'mongoose';
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
+import flash from 'connect-flash';
+import bodyParser from 'body-parser';
+
+// Definindo constantes
 const app = express();
 const port = 3000;
 
-//Conexão com o banco de dados
-import mongoose from "mongoose";
+// Conexão com o banco de dados
 const uri = process.env.CONNECTIONSTRING;
-mongoose.connect(uri) 
+mongoose.connect(uri)
   .then(() => {
-    app.emit('pronto')
+    app.emit('pronto');
     console.log('Conectado com sucesso');
   })
   .catch(err => console.log('Erro: ' + err));
 
-//Configuração e utilização de sessões
-import session from "express-session";
-import MongoStore from "connect-mongo";
-import flash from "connect-flash";
-
-function generateSessionSecrect(length){ //Gera o segredo de sessão de acordo com a quantidade de caracteres escolhida
+// Configuração e utilização de sessões
+function generateSessionSecret(length) {
   const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-  let secrect = '';
+  let secret = '';
   for (let i = 0; i < length; i++) {
     const randomIndex = Math.floor(Math.random() * charset.length);
-    secrect += charset[randomIndex];
+    secret += charset[randomIndex];
   }
-  return secrect;
+  return secret;
 }
-const sessionSecret = generateSessionSecrect(21); //Indica a quantidade de caracteres o segredo de sessão terá
+const sessionSecret = generateSessionSecret(21);
 
-let store = new MongoStore({
+const store = MongoStore.create({
   mongoUrl: uri,
   collectionName: "sessions"
-}) //Indica o banco de dados que será conectado, assim como a coleção em que as sessões serão salvas
+});
 
-const sessionOpt = session({ //Firma as configurações das sessões que serão criadas
+const sessionOpt = session({
   secret: sessionSecret,
   store: store,
   resave: false,
@@ -51,44 +54,39 @@ const sessionOpt = session({ //Firma as configurações das sessões que serão 
 app.use(sessionOpt);
 app.use(flash());
 
+// Usando Passport (importado de './controllers/passportLocal.js')
 import passport from "./controllers/passportLocal.js";
 app.use(passport.initialize());
 app.use(passport.session());
 
-//Devido a utilização do módulo ES, foi necessário a personalização da variável '__dirname'
-import path from 'path';
-import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// Permitindo requisições do corpo dos formulários
+app.use(bodyParser.urlencoded({ extended: true }));
 
-//Permite requisições do corpo dos formulários
-import bodyParser from 'body-parser';
-app.use(bodyParser.urlencoded({extended: true}));
-
-//Para visualização dos arquivos .ejs
-app.set('views', __dirname + '/views')
+// Para visualização dos arquivos .ejs
+app.set('views', path.join(new URL('.', import.meta.url).pathname, '/views'));
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/../public'));
 
-// app.engine('html', renderFile);
+// Definindo local dos arquivos estáticos
+const pathPublic = path.join(new URL('.', import.meta.url).pathname, '/../public');
+app.use(express.static(pathPublic));
 
-//Definindo rotas
+// Definindo rotas
 import profileRoute from './routes/profilePageRoute.js';
 import homepageRoute from './routes/homepageRoute.js';
 import loginRoute from './routes/loginRoute.js';
 import signinRoute from './routes/signInRoute.js';
 import signupRoute from './routes/signUpRoute.js';
 
-//Configurando Rotas
-app.use('/', homepageRoute)
-app.use('/profile', profileRoute)
-app.use('/login', loginRoute)
-app.use('/login/signin', signinRoute);  
-app.use('/login/signup', signupRoute); 
+// Configurando Rotas
+app.use('/', homepageRoute);
+app.use('/profile', profileRoute);
+app.use('/login', loginRoute);
+app.use('/login/signin', signinRoute);
+app.use('/login/signup', signupRoute);
 
-//o servidor começa a 'rodar' na porta, apenas após a conexão com o banco de dados
+// O servidor começa a 'rodar' na porta, apenas após a conexão com o banco de dados
 app.on('pronto', () => {
   app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
   });
-})
+}); 
