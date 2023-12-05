5ca670832f54d1a3dcc61025bd920245ba72bacd
Paulo Roberto <paulo.rcarlos23@gmail.com>
2023-12-05 00:44:31 -0300

    Importando resultado da lista para utilizá-lo na renderização da página home

--- src/routes/homepageRoute.js
+++ src/routes/homepageRoute.js
@@ -1,8 +1,18 @@
 import express from 'express';
+import { listSport } from '../controllers/sportController.js';
 var router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('pages/homepage.ejs')
-})
+router.get('/', async (req, res) => {
+    try {
+        const sportList = await listSport();
+
+        res.render('pages/homepage.ejs', {sportList});
+    } catch (error) {
+        console.error('Erro ao renderizar a página home:', error);
+        res.status(500).send('Erro interno do servidor');
+    }
+});
+
+router.get('/', listSport)
 
 export default router; 
