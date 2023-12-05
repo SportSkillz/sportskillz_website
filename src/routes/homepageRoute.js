7dd7d982db1c455ef216f15a7ad2a02f50f0c8e1
Paulo Roberto <paulo.rcarlos23@gmail.com>
2023-12-05 15:15:54 -0300

    criando contante que verifica a autenticação do usuário e passando a como parâmetro para renderizar a homepage

--- src/routes/homepageRoute.js
+++ src/routes/homepageRoute.js
@@ -5,8 +5,9 @@ var router = express.Router();
 router.get('/', async (req, res) => {
     try {
         const sportList = await listSport();
+        const isAuthenticated = req.isAuthenticated();
 
-        res.render('pages/homepage.ejs', {sportList});
+        res.render('pages/homepage.ejs', {sportList, isAuthenticated});
     } catch (error) {
         console.error('Erro ao renderizar a página home:', error);
         res.status(500).send('Erro interno do servidor');
