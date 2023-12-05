85e47cfe67e8acbda516fea807822e4372a75e83
Paulo Roberto <paulo.rcarlos23@gmail.com>
2023-12-04 22:56:15 -0300

    Configurando rota para lidar com o upload da imagem

--- src/routes/sportsRoute.js
+++ src/routes/sportsRoute.js
@@ -1,12 +1,17 @@
 import express from "express";
 import { createSport } from "../controllers/sportController.js";
+import multer from "multer";
 
 var router = express.Router();
+
+const storage = multer.memoryStorage(); // Armazena a imagem na memória
+const upload = multer({storage: storage});
 
 router.get('/', (req, res) => {
     res.render('sportForm');
 });
 
-router.post('/', createSport);
+// Rota para processar o envio do formulário com upload de imagem
+router.post('/', upload.single('image'), createSport);
 
 export default router;
