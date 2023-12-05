40707f45d2174d7328db8d68b5ef0b21824cad55
Paulo Roberto <paulo.rcarlos23@gmail.com>
2023-12-04 23:40:53 -0300

    Configurado com sucesso o salvamento dos uploads em um diretório local

--- src/controllers/sportController.js
+++ src/controllers/sportController.js
@@ -1,6 +1,7 @@
 import { SportModel } from "../models/sportModel.js";
 import fs from 'fs/promises';
 import path from 'path';
+import { fileURLToPath } from "url";
 
 export const createSport = async(req, res) => {
     try {
@@ -10,15 +11,18 @@ export const createSport = async(req, res) => {
             return res.status(400).json({erro: 'Dados invalidos'});
         }
 
+         // Utilizando fileURLToPath para obter o caminho do diretório atual
+         const currentDir = path.dirname(fileURLToPath(import.meta.url));
+
         // Cria um nome único para o arquivo
         const fileName = `${title.replace(/\s+/g, '_').toLowerCase()}_${Date.now()}${path.extname(req.file.originalname)}`;
 
         // Define o caminho onde o arquivo será salvo
-        const filePath = path.join(__dirname, '../../public/uploads', fileName);
+        const filePath = path.join(currentDir, '../../public/uploads', fileName);
 
          // Salva o arquivo no sistema de arquivos
         await fs.writeFile(filePath, req.file.buffer);
-        
+
         // Armazena o caminho da imagem no banco de dados
         const imageUrl = `/uploads/${fileName}`
 
