ae3077c2833013387d12c0ec006c7d877f9a9c6f
Paulo Roberto <paulo.rcarlos23@gmail.com>
2023-12-04 22:18:54 -0300

    Adicionando arquivo de rota para a utilizar a função 'createSport'

---
+++ src/routes/sportsRoutes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { createSport } from "../controllers/sportController";
+
+var router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('sportForm');
+});
+
+router.post('/', createSport);
