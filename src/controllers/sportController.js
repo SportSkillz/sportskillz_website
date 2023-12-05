5dce71afc76195bde0480df080ab262def875638
Paulo Roberto <paulo.rcarlos23@gmail.com>
2023-12-04 22:09:38 -0300

    Criando função CREATE para criar os esportes e armazená-los

---
+++ src/controllers/sportController.js
@@ -0,0 +1,18 @@
+import { SportModel } from "../models/sportModel";
+
+export const createSport = async(req, res) => {
+    try {
+        const {title, description} = req.body;
+
+        if(!title || !description){
+            return res.status(400).json({erro: 'Dados invalidos'});
+        }
+
+        const newSport = new SportModel({ title, description});
+
+        const savedSport = await newSport.save();
+        res.status(201).json(savedSport);
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao criar o esporte', details: error.message });
+    }
+}
